5de9a856818a97335892528e3fefa55d83047550
Matt Keas <matthiasak@gmail.com>
2014-12-11 12:13:28 -0600

    sweet

--- js/app.js
+++ js/app.js
@@ -8,18 +8,16 @@ function app(){
     // load some scripts (uses promises :D)
     loader.load(
         //css
-        {url:"./bower_components/normalize.css/normalize.css"},
-        {url:"./bower_components/typeplate-starter-kit/css/typeplate.css"},
-        {url: "./dist/style.css"},
+        {url: "../../dist/style.css"}
         //js
-        {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/dist/lodash.min.js"},
-        {url: "./bower_components/backbone/backbone.js"}
+        // {url: "../../bower_components/jquery/dist/jquery.min.js"},
+        // {url: "../../bower_components/lodash/dist/lodash.min.js"},
+        // {url: "../../bower_components/backbone/backbone.js"}
     ).then(function(){
-        _.templateSettings.interpolate = /{([\s\S]+?)}/g;
+        // _.templateSettings.interpolate = /{([\s\S]+?)}/g;
         document.body.style.opacity = 1;
         // start app?
     })
 
 }
-    
+
